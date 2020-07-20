04aec69c87ea74bec169c85c90171e591375900a
NetinqDev <sarquentin@gmail.com>
2020-07-21 00:03:14 +0200

    Adding header

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,12 @@
             <div class="elem">accueil</div>
         </a>
     </div>
-    <div class="col-md-8 menu">
-
+    <div class="col-md-8 menu menu-right">
+        <a href="#">
+            <div class="elem elem-btn">devenir annonceur</div>
+        </a>
+        <a href="#">
+            <div class="elem">me connecter</div>
+        </a>
     </div>
 </header>
