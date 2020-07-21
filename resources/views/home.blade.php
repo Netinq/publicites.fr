6e6fdfb94c88ff6dde15a7ce8ea0a80b227c54fe
NetinqDev <sarquentin@gmail.com>
2020-07-21 14:19:06 +0200

    Updating

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app', ['styles' => ['pages/home'], 'scripts' => ['home']])
 
 @section('content')
-<div id="content">
+<div id="content-image">
     <img class="background background-active" src="{{asset('images/departements/auvergne-rhone-alpes.jpg')}}">
     <img class="background" src="{{asset('images/departements/bourgogne-franche-compte.jpg')}}">
     <img class="background" src="{{asset('images/departements/centre-val-de-loire.jpg')}}">
@@ -14,4 +14,13 @@
     <img class="background" src="{{asset('images/departements/provence-alpes-cote-d-azur.jpg')}}">
     <div id="mask"></div>
 </div>
+<div>
+
+</div>
+<footer>
+    <div class="elem d-none d-md-inline-block">tous droits réservés</div>
+    <a href="#">
+        <div class="elem">cgv - mentions légales</div>
+    </a>
+</footer>
 @endsection
