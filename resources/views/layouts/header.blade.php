a6280645a5c9447cd1ca1f58defcfd2aa76fcadb
NetinqDev <sarquentin@gmail.com>
2020-07-21 13:45:40 +0200

    Home resposive version

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -1,16 +1,27 @@
 <header class="row">
-    <div class="col-md-4 menu">
+    <div class="col-md-4 menu d-none d-md-flex">
         <img id="logo" alt="Website logo" src="{{asset('images/logo.png')}}">
         <a href="{{route('home')}}">
             <div class="elem">accueil</div>
         </a>
     </div>
-    <div class="col-md-8 menu menu-right">
+    <div class="col-md-8 menu menu-right d-none d-md-flex">
         <a href="#">
             <div class="elem elem-btn">devenir annonceur</div>
         </a>
         <a href="#">
             <div class="elem">me connecter</div>
+        </a>
+    </div>
+    <div class="col-12 menu menu-m d-md-none">
+        <img id="logo" alt="Website logo" src="{{asset('images/logo.png')}}">
+        <a href="#">
+            <div class="elem">devenir annonceur</div>
+        </a>
+    </div>
+    <div class="col-12 menu menu-b d-md-none">
+        <a href="#">
+            <div class="elem">connexion</div>
         </a>
     </div>
 </header>
